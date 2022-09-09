f151174b300f493f17f5ae68f99265598d13dec2
Godwinugo <godwinugo2@gmail.com>
2022-09-08 18:56:29 -0700

    my 4th commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,30 @@
-#!/bin/bash
-int puts(const char *Programming is like building a multilingual puzzle) 0
+#include <stdio.h>
+
+
+
+/**
+
+* main - print the string in the put function
+
+*
+
+* Description: using the main function
+
+* this program prints "Programming is like building a multilingual puzzle
+
+* Return: 0
+
+*/
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
+
+
+
