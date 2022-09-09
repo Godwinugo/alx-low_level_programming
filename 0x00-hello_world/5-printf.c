524773b786a3ca411335932bcb585ff1b3472bcb
Godwinugo <godwinugo2@gmail.com>
2022-09-09 04:06:09 -0700

    my 5th commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -26,3 +26,17 @@ int main(void)
 	return (0);
 
 }
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "with proper grammar, but the outcome is a piece of art"
+* Return: 0
+*/
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
