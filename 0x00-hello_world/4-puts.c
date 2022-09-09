1e19e99e45e3a7b36ea4a51057ea803e4135b665
Godwinugo <godwinugo2@gmail.com>
2022-09-09 03:50:20 -0700

    my 4th commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,14 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
 
-	return (0);
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+         puts("\"Programming is like building a multilingual puzzle");
+         return (0);
+}
