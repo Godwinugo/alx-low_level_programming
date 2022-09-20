e6032c9877eaa8ad4ae8a7b5ee1b8a5dbbdbb2dd
Godwinugo <godwinugo2@gmail.com>
2022-09-19 20:58:47 -0700

    my 10th commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,6 +11,7 @@ int _atoi(char *s)
 {
 int sign = 1;
 unsigned int num = 0;
+
 do {
 if (*s == '-')
 sign *= -1;
@@ -19,5 +20,6 @@ num = num * 10 + (*s - '0');
 else if (num > 0)
 break;
 } while (*s++);
+
 return (num * sign);
 }
