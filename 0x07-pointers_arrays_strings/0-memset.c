e055ba28a132c003922a16a9e376969314ea72de
Godwinugo <godwinugo2@gmail.com>
2022-09-27 15:05:33 -0700

    my zeroth commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,9 +3,8 @@
 /**
 *_memset - fills a memory block with a constant byte
 *@s: address to memory block
-*@b: char to be used 
+*@b: char to be used
 *@n: number of bytes to be used
-*
 *Return: pointer to the memory block
 */
 
@@ -15,8 +14,8 @@ char *_memset(char *s, char b, unsigned int n);
 while (n)
 
 {
-s[n -1] = b;
+s[n - 1] = b;
 n--;
 }
-return (s):
+return (s);
 }
